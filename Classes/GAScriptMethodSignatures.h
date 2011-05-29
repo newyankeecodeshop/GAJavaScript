8cc9a37c6d62ca8d3c04508eeffba3ee3bacd012
Andrew Goodale <andrewhgoodale@gmail.com>
2011-05-29 10:42:51 -0600

    Support NSInvocation for function arguments. Fixed date conversion.

---
+++ Classes/GAScriptMethodSignatures.h
@@ -0,0 +1,32 @@
+//
+//  GAScriptMethodSignatures.h
+//  GAJavaScript
+//
+//  Created by Andrew on 5/29/11.
+//  Copyright 2011 Wingspan Technology, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface GAScriptMethodSignatures : NSObject 
+{
+}
+
+/**
+ * Adds method signatures from the given class to the list of known signatures.
+ */
++ (void)addMethodSignaturesForClass:(Class)clazz;
+
+/**
+ * Used by the script object to find a method signature for the given selector.
+ */
++ (NSMethodSignature *)findMethodSignatureForSelector:(SEL)aSelector;
+
+#pragma mark DOM Methods
+
+- (id)getElementById:(NSString *)elementId;
+
+- (id)getElementsByTagName:(NSString *)tagName;
+
+@end
---
+++ Classes/GAScriptMethodSignatures.m
@@ -0,0 +1,57 @@
+//
+//  GAScriptMethodSignatures.m
+//  GAJavaScript
+//
+//  Created by Andrew on 5/29/11.
+//  Copyright 2011 Wingspan Technology, Inc. All rights reserved.
+//
+
+#import "GAScriptMethodSignatures.h"
+
+static NSMutableSet* s_classes = nil;
+
+@implementation GAScriptMethodSignatures
+
++ (void)addMethodSignaturesForClass:(Class)clazz
+{
+    if (s_classes == nil)
+    {
+        s_classes = [[NSMutableSet alloc] initWithCapacity:16];
+		[s_classes addObject:[GAScriptMethodSignatures class]];
+	}
+	
+	[s_classes addObject:clazz];
+}
+
++ (NSMethodSignature *)findMethodSignatureForSelector:(SEL)aSelector
+{
+    if (s_classes == nil)
+    {
+        s_classes = [[NSMutableArray alloc] initWithCapacity:16];
+		[s_classes addObject:[GAScriptMethodSignatures class]];
+	}
+    
+    // If performance becomes a concern, we can build a cache of these mappings.
+    //
+    for (Class aClass in s_classes)
+    {
+        if ([aClass instancesRespondToSelector:aSelector])
+            return [aClass instanceMethodSignatureForSelector:aSelector];
+    }
+	
+    return nil;
+}
+
+#pragma mark DOM Methods
+
+- (id)getElementById:(NSString *)elementId
+{
+	return nil;
+}
+
+- (id)getElementsByTagName:(NSString *)tagName
+{
+	return nil;
+}
+
+@end
