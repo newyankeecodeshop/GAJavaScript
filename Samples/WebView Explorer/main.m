36534abb480a4ec83091e2dc01c22da0f4275380
Andrew Goodale <andrewhgoodale@gmail.com>
2011-07-06 10:46:54 -0400

    Sample that shows hot to introspect the DOM

---
+++ Samples/WebView Explorer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WebView Explorer
+//
+//  Created by Andrew Goodale on 6/5/11.
+//  Copyright 2011 Wingspan Technology, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
